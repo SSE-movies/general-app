7049eb4dc2c227b95afdde8730ee14a13bfaa394
Tiago <tiagoriely@gmail.com>
2025-02-09 16:23:11 +0000

    Created the templates directory and added a simple index.htlm. Also updated the PORT to 80.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_movies():
- return "Hello, this web app will provide more information about movies"
+ return render_template("index.html")
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=80)
+# Run Flask app on port 80, accessible from any IP
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
