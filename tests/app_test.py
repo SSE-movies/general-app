33c172d12bb08adc3edccaec049b0fcd3072dfb7
ZevMn <zevmenachemson@gmail.com>
2025-02-10 15:56:15 +0000

    Created tests directory and app_test.py empty file

---
+++ tests/app_test.py
@@ -0,0 +1,11 @@
+from ..app import app
+# import requests
+
+# test correct loading of homepage
+def test_homepage_loads_correctly():
+    # Create a test client to allow simulation of HTTP requests to the app
+    with app.test_client() as client:
+        response = client.get("/")
+
+    # Assert the request succeeded (HTTP 200)
+    assert response.status_code == 200
