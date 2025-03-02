aef13fc318025a576495bdcb21bf1c3431edeafc
ZevMn <zevmenachemson@gmail.com>
2025-03-02 12:19:09 +0000

    Edited () to pass linting code quality check

--- src/database.py
+++ src/database.py
@@ -84,8 +84,8 @@ def get_filtered_movies(query_params=None, username=None):
             movie["in_watchlist"] = movie["showId"] in watchlist_movies
 
         # Determine pagination flags
-        has_next = (len(movies) == results_per_page)  # assume a next page if full page was returned
-        has_prev = (page > 1)
+        has_next = len(movies) == results_per_page  # assume a next page if full page was returned
+        has_prev = page > 1
 
         # Don't return total for now
         return movies, page, has_next, has_prev, None
